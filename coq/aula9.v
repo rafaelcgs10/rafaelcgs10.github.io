525ebb12636663411ade18e7c4d729cd7dd86980
rafaelcgs10 <rafaelcgs10@gmail.com>
2018-05-18 20:20:56 -0300

    aula 9 begins

---
+++ coq/aula9.v
@@ -0,0 +1,28 @@
+(** **** Questão discursiva abaixo!  *)
+(** **** Exercise: 2 stars (mumble_grumble)  *)
+(** Consider the following two inductively defined types. *)
+
+Inductive mumble : Type :=
+  | a : mumble
+  | b : mumble -> nat -> mumble
+  | c : mumble.
+
+Inductive grumble (X:Type) : Type :=
+  | d : mumble -> grumble X
+  | e : X -> grumble X.
+
+(** Which of the following are well-typed elements of [grumble X] for
+    some type [X]?
+      - [d (b a 5)]
+      - [d mumble (b a 5)]
+      - [d bool (b a 5)]
+      - [e bool true]
+      - [e mumble (b c 0)]
+      - [e bool (b c 0)]
+      - [c]
+(* FILL IN HERE *)
+*)
+(** [] *)
+
+End MumbleGrumble.
+
