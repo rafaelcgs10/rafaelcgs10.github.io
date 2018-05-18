be6089ac41734b026daf1946ae42786e1e262b5d
rafaelcgs10 <rafaelcgs10@gmail.com>
2018-05-18 16:46:43 -0300

    aula 8 quase completa

---
+++ coq/doit6.v
@@ -0,0 +1,71 @@
+(** **** Exercise: 3 stars (list_exercises)  *)
+(** More practice with lists: *)
+
+Theorem app_nil_r : forall l : natlist,
+  l ++ [] = l.
+Proof.
+  (* FILL IN HERE *) Admitted.
+
+Theorem rev_app_distr: forall l1 l2 : natlist,
+  rev (l1 ++ l2) = rev l2 ++ rev l1.
+Proof.
+  (* FILL IN HERE *) Admitted.
+
+Theorem rev_involutive : forall l : natlist,
+  rev (rev l) = l.
+Proof.
+  (* FILL IN HERE *) Admitted.
+
+(** There is a short solution to the next one.  If you find yourself
+    getting tangled up, step back and try to look for a simpler
+    way. *)
+
+Theorem app_assoc4 : forall l1 l2 l3 l4 : natlist,
+  l1 ++ (l2 ++ (l3 ++ l4)) = ((l1 ++ l2) ++ l3) ++ l4.
+Proof.
+  (* FILL IN HERE *) Admitted.
+
+(** An exercise about your implementation of [nonzeros]: *)
+
+Lemma nonzeros_app : forall l1 l2 : natlist,
+  nonzeros (l1 ++ l2) = (nonzeros l1) ++ (nonzeros l2).
+Proof.
+  (* FILL IN HERE *) Admitted.
+(** [] *)
+
+(** **** Exercise: 2 stars (beq_natlist)  *)
+(** Fill in the definition of [beq_natlist], which compares
+    lists of numbers for equality.  Prove that [beq_natlist l l]
+    yields [true] for every list [l]. *)
+
+Fixpoint beq_natlist (l1 l2 : natlist) : bool
+  (* REPLACE THIS LINE WITH ":= _your_definition_ ." *). Admitted.
+
+Example test_beq_natlist1 :
+  (beq_natlist nil nil = true).
+ (* FILL IN HERE *) Admitted.
+
+Example test_beq_natlist2 :
+  beq_natlist [1;2;3] [1;2;3] = true.
+(* FILL IN HERE *) Admitted.
+
+Example test_beq_natlist3 :
+  beq_natlist [1;2;3] [1;2;4] = false.
+ (* FILL IN HERE *) Admitted.
+
+Theorem beq_natlist_refl : forall l:natlist,
+  true = beq_natlist l l.
+Proof.
+  (* FILL IN HERE *) Admitted.
+(** [] *)
+
+(** **** Exercise: 4 stars, advanced (rev_injective)  *)
+(** Prove that the [rev] function is injective -- that is,
+
+    forall (l1 l2 : natlist), rev l1 = rev l2 -> l1 = l2.
+
+(There is a hard way and an easy way to do this.) *)
+
+(* FILL IN HERE *)
+(** [] *)
+
