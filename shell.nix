444f5481af51dde3600cd47541020771969ca613
rafaelcgs10 <rafaelcgs10@gmail.com>
2022-06-28 14:27:44 +0200

    Fixes

--- shell.nix
+++ shell.nix
@@ -1,19 +1,14 @@
 with (import <nixpkgs> {});
 
-let
-  unstable = import
-    (builtins.fetchTarball https://github.com/NixOS/nixpkgs/archive/93184413f2180ce79d53df91df4d43c2e8f931aa.tar.gz)
-    { inherit config; };
-in
-unstable.mkShell {
+mkShell {
   name = "aspell-env";
   buildInputs = [
-    (pkgs.aspellWithDicts (d: [d.en]))
-    unstable.jre
-    unstable.languagetool
-    unstable.editorconfig-core-c
-    unstable.texlab
-    (unstable.texlive.combine { inherit (unstable.texlive) scheme-small wrapfig capt-of preview import enumitem; })
+    (aspellWithDicts (d: [d.en]))
+    jre
+    languagetool
+    editorconfig-core-c
+    texlab
+    (texlive.combine { inherit (texlive) scheme-small wrapfig capt-of preview import enumitem; })
   ];
   paths = [aspell aspellDicts.en];
 }
